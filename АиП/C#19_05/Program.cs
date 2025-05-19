2925317742afb2ca0ddd1fa95ce52c06198f872f
Aleksandr12234 <566566sana@gmail.com>
2025-05-19 09:34:59 +0600

    поправка кода 19.05

--- АиП/C#19_05/Program.cs
+++ АиП/C#19_05/Program.cs
@@ -1,23 +1,25 @@
-﻿using System.Drawing;
-
-class Program
+﻿class Program
 {
-    static int[] massive=new int[]{10, 12, 23, 98, 123, 12341, 1234567, 1223, 123456, 123455432, 01210, -121, 1234123};
 
     unsafe private static void Main(string[] args)
     {
-        foreach(int i in massive){
-            int* ssilka=&i;
-            string aktive = "" + *ssilka;
 
-            for(int j=0; j<aktive.Length/2; j++){
-                if(aktive[j]!=aktive[aktive.Length-1-j]){
+        int* massive = stackalloc int[] {10, 1, 22, 40};
+
+        for (int i= 0; i< 4; i++)
+        {
+            string aktive = "" + massive[i];
+
+            for (int j = 0; j < aktive.Length / 2; j++)
+            {
+                if (aktive[j] != aktive[aktive.Length - 1 - j])
+                {
                     goto Point;
                 }
             }
 
-            Console.Write(*ssilka+" ");
-            Point:;
+            Console.Write(massive[i] + " ");
+        Point:;
         }
         
         Console.WriteLine();
---
+++ Дискретная_математика/allCode/src/java_10_03/FordBelman.java
@@ -0,0 +1,5 @@
+package java_10_03;
+
+public class FordBelman {
+    
+}
